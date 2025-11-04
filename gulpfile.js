eeeac5773a821cd7848f0fb5cd7e5fb20458c547
Suellen Santana <suellensantana18@gmail.com>
2025-11-03 23:19:05 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+// Compila SCSS  CSS
+function styles() {
+  return gulp.src("./src/styles/main.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("./dist/css"));
+}
+
+// Observa alterações
+function watchFiles() {
+  gulp.watch("./src/styles/*.scss", styles);
+}
+
+// Exporta tasks
+exports.styles = styles;
+exports.watch = watchFiles;
+exports.default = styles;
